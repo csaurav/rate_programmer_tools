21cce1a54c6286bab313bd7cfba8702495ce6423
saurabh <saurabh@saurabh.(none)>
2013-01-26 10:54:37 -0800

    Rspec test passing. Fixed for Ajax requests

--- spec/features/search_for_tools_spec.rb
+++ spec/features/search_for_tools_spec.rb
@@ -2,18 +2,14 @@ require 'spec_helper'
 
 feature "From the home page, a user should be able to search for tools." do
 
-  scenario  "If a user enters a search term and the tool does
-not exist, the user should see a message saying so." do
-
+  scenario  "If a user enters a search term and the tool does not exist, the user should see a message saying so.", js: true do
     visit '/'
     fill_in "search_field", with: "NHibernate"
     click_on "Search"
     page.should have_content("No tool was found matching that name")
   end
 
-  scenario "If a user enters a search term and the tool exists, display the
-tool in the search results" do
-
+  scenario "If a user enters a search term and the tool exists, display the tool in the search results", js: true do
    Tool.create!(name: "NHibernate")
     visit '/'
     fill_in "search_field", with: "NHibernate"
