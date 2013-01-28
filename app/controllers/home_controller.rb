eda81c2be48cbce7edc5b0fc0c7a6258dc2e6890
Kirmanie L Ravariere <enamrik@gmail.com>
2013-01-28 08:51:33 -0900

    remove premature layering/architecting in the spirit of Ruby

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,17 +1,9 @@
 class HomeController < ApplicationController
-  include RateProgrammerTools
 
   def search
-    @result_model = ResultModel.new
-    tools = Tool.where('name LIKE ?', "%#{params[:search_field]}%").all
-    if tools.count > 0
-      @result_model.model = tools
-    else
-      @result_model.add_error "No tool was found matching that name"
-    end
+    @tools = Tool.where('name LIKE ?', "%#{params[:search_field]}%").all
     respond_to  do |format|
       format.js { render :tool_result_set }
-      format.html { render :index }
     end
   end
 end
