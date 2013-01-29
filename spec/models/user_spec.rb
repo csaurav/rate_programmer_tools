cd0c541aca05a04eb0bee415159e28bdc05a6ff3
saurabh <saurabh@saurabh.(none)>
2013-01-28 19:33:15 -0800

    User validations all pass

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,63 +2,93 @@ require 'spec_helper'
 require 'faker'
 describe 'User' do
 	it 'has a valid email' do
-		username = Faker::Name.first_name + '_' +Faker::Name.last_name
+		username = 'fake_username'
 		password = 'some_password'
-		user = User.create username: username, password: password
+		user = User.new username: username, password: password, email: 'valid_email@gmail.com',
+										password_confirmation: password
+		user.save!
 
-		user.update_attributes email: (Faker::Base.regexify  /^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$/ )				
+		user.email =  (Faker::Base.regexify  /^[A-Z_a-z0-9_]{5,10}@[a-zA-Z0-9]{2,5}.[a-zA-Z]{2,4}$/)
 		user.should be_valid
 
 		user.update_attributes 	email: 'notAValidEmail' 
 		user.should_not be_valid
 
-		user.update_attributes email: nil
+		user.email =  nil
 		user.should_not be_valid
+
+		user.email = 'Valid_Email@Gmail.Com'
+		user.save!
+		duplicateEmail = User.new username: 'new_user', password: password, email: 'valid_email@gmail.com',
+										password_confirmation: password
+		duplicateEmail.should_not be_valid
+
 	end
 
 	it 'has a valid username' do
 		email =  'valid_email@gmail.com'
 		password = 'some_password'
-		user = User.create email: email, password: password
+		user = User.new username: 'valid_username', email: email, password: password,password_confirmation: password
+		user.save!
 
-		user.update_attributes username: (Faker::Base.regexify /^[a-z0-9_-]{5,15}$/)
+		user.username =  (Faker::Base.regexify /^[a-z0-9_-]{5,15}$/)
 		user.should be_valid
 
 		#valid symbols = only underscore
-		user.update_attributes username: '$@!====invalid_username'
+		user.username =  '$@!====invalid_username'
 		user.should_not be_valid
 
 		#at least 5 characters
-		user.update_attributes username: 'hi'
+		user.username =  'hi'
 		user.should_not be_valid
 
 		#at most 15 characters
-		user.update_attributes username: '012345678912356'
+		user.username =  '1234567890123456'
 		user.should_not be_valid
 
 		#MUST have a username
-		user.update_attributes username: nil
+		user.username =  nil
 		user.should_not be_valid
+
+		#username must be unique
+		user.username =  'duplicateUsername'
+		duplicate = User.new email: email, password: password, username: 'DupLiCateUsername',password_confirmation: password
+		duplicate.should_not be_valid
 	end
 
 	it 'has a valid password' do
-		username = Faker::Name.first_name + '_' +Faker::Name.last_name
+		username = 'fake_username'
 		email = 'valid_email@gmail.com'
-		user = User.create username: username, email: email
+		user = User.new username: username, email: email, password: 'test_password', password_confirmation: 'test_password'
+		user.save! 
 
-		user.update_attributes password: nil
+		user.password =  nil
 		user.should_not be_valid
 
-	#	user.update_attributes password: (Faker::Base.regexify %r-[\d\w!@#$%^&*()+-='[\]>?"{}]{5,20}-)
-		#user.should be_valid
-
-		user.update_attributes password: '!@#$%^&*()_+-=;\'{}[]:"<>,.?/'
+		user.password =  '!@#$%^&*()_+-=;\'{}[]:"<>,.?/'
+		user.password_confirmation = '!@#$%^&*()_+-=;\'{}[]:"<>,.?/'
 		user.should be_valid
 
-		user.update_attributes password: 'aValidPassword1234567890'
+		user.password =  'aValidPassword1234567890'
+		user.password_confirmation = 'aValidPassword1234567890'
 		user.should be_valid
 
-		user.update_attributes password: '\\password'
+		user.password =  '\\password'
+		user.password_confirmation = '\\password'
+		user.should be_valid
+	end
+
+	it 'password confirmation works' do
+		username = 'fake_username'
+		email = 'valid_email@gmail.com'
+		user = User.new username: username, email: email, password: 'test_password', password_confirmation: 'test_password'
+		user.save!
+
+		user.password_confirmation = 'non_working'
 		user.should_not be_valid
+
+		user.password_confirmation = nil
+		user.should_not be_valid
+
 	end
 end
